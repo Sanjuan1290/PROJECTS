40b1f19fe66dfbaf13a25988f0c091344850840c
robert <robertrenbysanjuan@gmail.com>
2025-04-24 09:29:16 +0800

    add: clothSize image on products that are in cloths category

--- AmazonClone/scripts/amazonScripts/main.js
+++ AmazonClone/scripts/amazonScripts/main.js
@@ -5,44 +5,90 @@ export function renderProducts(products){
     main_grid_elem.innerHTML = ``;
     products.forEach((product) => {
 
-        main_grid_elem.innerHTML +=     
-        `
-            <div class="content-container">
+       if(!product.type){
+            main_grid_elem.innerHTML +=     
+            `
+                <div class="content-container">
 
-                <div class="product-image-container"><img class="product-image" src="./${product.image}" alt="productImage"></div>
+                    <div class="product-image-container"><img class="product-image" src="./${product.image}" alt="productImage"></div>
 
-                <div class="content-product">
-                    <p class="product-name">${product.name}</p>
+                    <div class="content-product">
+                        <p class="product-name">${product.name}</p>
 
-                    <div class="rating-container">
-                        <img src="./images/ratings/rating-${product.rating.stars * 10}.png" alt="rating">
-                        <p>${product.rating.count}</p>
+                        <div class="rating-container">
+                            <img src="./images/ratings/rating-${product.rating.stars * 10}.png" alt="rating">
+                            <p>${product.rating.count}</p>
+                        </div>
+
+                        <p class="product-price">$${(product.priceCents / 100).toFixed(2)}</p>
+                        
+                        <select class="select-product-quantity js-select-product-quantity-${product.id}">
+                            <option value="1" selected>1</option>
+                            <option value="2" >2</option>
+                            <option value="3" >3</option>
+                            <option value="4" >4</option>
+                            <option value="5" >5</option>
+                            <option value="6" >6</option>
+                            <option value="7" >7</option>
+                            <option value="8" >8</option>
+                            <option value="9" >9</option>
+                            <option value="10" >10</option>
+                        </select>
+
+                        <div class="checkmark-added js-checkmark-added-${product.id}">
+                            <img src="./images/icons/checkmark.png" alt="Checkmark">
+                            <p>Added</p>
+                        </div>
+
+                        <button class="add-to-cart-btn" data-product-id="${product.id}">Add to cart</button>
                     </div>
-
-                    <p class="product-price">$${(product.priceCents / 100).toFixed(2)}</p>
-                    
-                    <select class="select-product-quantity js-select-product-quantity-${product.id}">
-                        <option value="1" selected>1</option>
-                        <option value="2" >2</option>
-                        <option value="3" >3</option>
-                        <option value="4" >4</option>
-                        <option value="5" >5</option>
-                        <option value="6" >6</option>
-                        <option value="7" >7</option>
-                        <option value="8" >8</option>
-                        <option value="9" >9</option>
-                        <option value="10" >10</option>
-                    </select>
-
-                    <div class="checkmark-added js-checkmark-added-${product.id}">
-                        <img src="./images/icons/checkmark.png" alt="Checkmark">
-                        <p>Added</p>
-                    </div>
-
-                    <button class="add-to-cart-btn" data-product-id="${product.id}">Add to cart</button>
                 </div>
-            </div>
-         `;
+            `;
+        } else if(product.type == 'clothing'){
+            main_grid_elem.innerHTML +=     
+            `
+                <div class="content-container">
+    
+                    <div class="product-image-container"><img class="product-image" src="./${product.image}" alt="productImage"></div>
+    
+                    <div class="content-product">
+                        <p class="product-name">${product.name}</p>
+    
+                        <div class="rating-container">
+                            <img src="./images/ratings/rating-${product.rating.stars * 10}.png" alt="rating">
+                            <p>${product.rating.count}</p>
+                        </div>
+    
+                        <p class="product-price">$${(product.priceCents / 100).toFixed(2)}</p>
+                        
+                        <select class="select-product-quantity js-select-product-quantity-${product.id}">
+                            <option value="1" selected>1</option>
+                            <option value="2" >2</option>
+                            <option value="3" >3</option>
+                            <option value="4" >4</option>
+                            <option value="5" >5</option>
+                            <option value="6" >6</option>
+                            <option value="7" >7</option>
+                            <option value="8" >8</option>
+                            <option value="9" >9</option>
+                            <option value="10" >10</option>
+                        </select>
+    
+                        <div class="checkmark-added js-checkmark-added-${product.id}">
+                            <img src="./images/icons/checkmark.png" alt="Checkmark">
+                            <p>Added</p>
+                        </div>
+
+                        <p class="clothSizeText">CLoth Size</p>
+
+                        <button class="add-to-cart-btn" data-product-id="${product.id}">Add to cart</button>
+                    </div>
+                </div>
+             `;
+
+
+            document.querySelector('body').add
+        }
     });
 
     document.querySelectorAll(`.add-to-cart-btn`).forEach(btn => {
@@ -56,6 +102,8 @@ export function renderProducts(products){
         })
         
     })
+
+    handleWarrantyClick()
     showCheckMark();
 }
 
@@ -85,3 +133,44 @@ function showCheckMark(){
     
 }
 
+let isClothSizeOpen = false;
+function handleWarrantyClick(){
+    document.querySelectorAll('.clothSizeText').forEach(warrant => {
+        warrant.addEventListener('click', ()=>{
+
+            if(isClothSizeOpen) return
+
+            const clothImageContainer_elem = document.createElement('div')
+            clothImageContainer_elem.classList.add('clothImageContainer')
+    
+            clothImageContainer_elem.innerHTML += `
+                <button class="closeSizeImageBtn">X</button>
+            `;
+    
+            const clothSizeImage = document.createElement('img')
+            clothSizeImage.src = '././././images/instruction/clothingSize.png'
+            clothSizeImage.classList.add('clothSize')
+    
+            clothImageContainer_elem.append(clothSizeImage)
+    
+            document.querySelector('body').append(clothImageContainer_elem)
+    
+            main_grid_elem.style.opacity = .5;
+            
+            document.querySelector('.closeSizeImageBtn').addEventListener('click' , ()=>{
+                handleCloseSizeImageClick(clothImageContainer_elem)
+            })
+
+            isClothSizeOpen = true;
+        })
+    })
+}
+
+
+function handleCloseSizeImageClick(clothImageContainer_elem){
+    main_grid_elem.style.opacity = 1;
+
+    clothImageContainer_elem.remove()
+
+    isClothSizeOpen = false;
+}
